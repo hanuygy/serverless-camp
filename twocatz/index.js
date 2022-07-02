06b546f687098dbd0388b9d97ec7842069c71324
Josephine Huang <johuan147@gmail.com>
2022-07-01 23:14:53 -0400

    v7

--- twocatz/index.js
+++ twocatz/index.js
@@ -3,11 +3,11 @@ const fetch = require('node-fetch')
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    let resp = await fetch("https://bit-cat.azurewebsites.net/cat/says/serverless", {
+    const resp = await fetch("https://bit-cat.azurewebsites.net/cat/says/serverless", {
         method: 'GET'
     });
     
-    let data = await resp.arrayBuffer()
+    const data = await resp.arrayBuffer()
     // we need to receive it as a buffer since this is an image we are receiving from the API
     // Buffer?? https://developer.mozilla.org/en-US/docs/Web/API/Blob
 
