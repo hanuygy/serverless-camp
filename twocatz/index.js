661333032a0cd11fbeafd67a5a82a6c2ebba8520
Josephine Huang <johuan147@gmail.com>
2022-07-01 23:24:30 -0400

    8

--- twocatz/index.js
+++ twocatz/index.js
@@ -12,8 +12,8 @@ module.exports = async function (context, req) {
     // Buffer?? https://developer.mozilla.org/en-US/docs/Web/API/Blob
 
     var base64data = Buffer.from(data).toString('base64')
-    //put what you want to turn into base64 inside "originaldata"
-    //"originaldata" will be encoded in base64.
+    // put what you want to turn into base64 inside "originaldata"
+    // "originaldata" will be encoded in base64.
 
     context.res = {
         // status: 200, /* Defaults to 200 */
