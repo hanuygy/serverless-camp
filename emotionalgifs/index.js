6254e1b252e8017cbc60db3b238f4f3ad5a2507a
Josephine Huang <johuan147@gmail.com>
2022-07-03 18:10:15 -0400

    emotional gifs

--- emotionalgifs/index.js
+++ emotionalgifs/index.js
@@ -1,4 +1,5 @@
 const multipart = require('parse-multipart');
+const fetch = require('node-fetch');
 
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
@@ -12,11 +13,42 @@ module.exports = async function (context, req) {
     // parse the body
     const parts = multipart.Parse(body, boundary);
     
-    let convertedResult = Buffer.from(parts[0].data).toString('base64');
-    // FILL IN THE BLANK
-
+    //module.exports function
+    //analyze the image
+    const result = await analyzeImage(parts[0].data);
     context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: convertedResult
+        body: {
+            result
+        }
     };
+    console.log(result)
+    context.done(); 
 }
+
+async function analyzeImage(img){
+    const subscriptionKey = process.env.SUBSCRIPTIONKEY;
+    const uriBase = process.env.ENDPOINT + '/face/v1.0/detect';
+    
+
+    let params = new URLSearchParams({
+        'returnFaceId': 'true',
+        'returnFaceAttributes': 'emotion'     //FILL IN THIS LINE
+    })
+
+    //COMPLETE THE CODE
+    let resp = await fetch(uriBase + '?' + params.toString(), {
+        method: 'POST',  //WHAT TYPE OF REQUEST?
+        body: img,  //WHAT ARE WE SENDING TO THE API?
+        
+            //ADD YOUR TWO HEADERS HERE
+        headers: {
+            'Content-Type': 'application/octet-stream',
+            'Ocp-Apim-Subscription-Key': subscriptionKey
+        }
+    })
+
+    let data = await resp.json();
+
+    return data;
+}
+
